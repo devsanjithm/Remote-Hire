7cddaac8472a236ec73a4c4d6c9aa4e969d30bf7
SANJITH M <dev.sanjithm@gmail.com>
2022-04-30 17:15:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Navigate, Route } from "react-router-dom";
+import Login from "./Component/login"
+import Signup from "./Component/signup";
+import Home from "./Component/Home";
+import { ContextProvider } from "./context";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ContextProvider>
+			<Router>
+				<Routes>
+					<Route exact path="/" element={<Navigate to="/login" />} />
+					<Route exact path="/login" element={<Login />} />
+					<Route exact path="/signup" element={<Signup />} />
+					<Route exact path="/Home" element={<Home />} />
+				</Routes>
+			</Router>
+		</ContextProvider>
+	);
 }
 
 export default App;
