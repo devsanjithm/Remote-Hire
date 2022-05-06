e7da20927c6e87cd206924fe8f4aa33f6b441a62
SANJITH M <dev.sanjithm@gmail.com>
2022-05-06 09:24:13 +0530

    sudev (#3)
    Co-authored-by: vishnusudev <vishnusudev2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import Home from "./Component/Home";
 import Add from "./Component/Add";
 import Hrpage from "./Component/hrpage";
 import Admin from "./Component/Admin";
+import Aprove from "./Component/Approve";
 import { ContextProvider } from "./context";
 import 'react-toastify/dist/ReactToastify.css';
+import Approve from "./Component/Approve";
 
 function App() {
 	const { user, setloadscreen, loadscreen, userroll, setuseroll } = useContext(UserContext);
@@ -24,6 +26,7 @@ function App() {
 					<Route exact path="/Home" element={<Home />} />
 					<Route exact path="/Add" element={<Add />} />
 					<Route exact path="/admin" element={<Admin />} />
+					<Route exact path="/Approve" element={<Approve />} />
 				</Routes>
 			</Router>
 		</ContextProvider>
--- src/Component/Approve.js
+++ src/Component/Approve.js
@@ -1,22 +1,19 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
-function Admin() {
+function Approve() {
 
     const [BtnToggle, setbtntoggle] = useState(true);
     const navigate = useNavigate();
     const data = [
 
         {
-            jobrole: "Software Manager",
-            jobAddress: "Fulfillment Center Services",
-            jobmode: "Remote",
-            jobsalary:
-            {
-                from: "200,000.00",
-                to: "1,200,129.00"
-            },
-            jobspec: " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries",
+            name: "Sanjith",
+            Age: "Age: 38",
+            desg: "Software developer",
+            Experience:"12 years",
+            Domain:"Web development",
+            
         }
 
     ]
@@ -33,26 +30,29 @@ function Admin() {
                 </div>
 
                 <div class="hidden md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0" id="navbar-collapse">
+                        {/* <div
+                            className="px-3 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
+                        >
+                            <button
+                                onClick={() => {
+                                    navigate("/Approve")
+                                }}
+                                class="bg-indigo-700 shadow-lg hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                                Add / Remove Hr
+                            </button>
+                        </div> */}
                     <div
                         className="px-3 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
                     >
                         <button
-                            onClick={() => {
-                                navigate("/Approve")
-                            }}
-                            class="bg-indigo-700 shadow-lg hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                            Add / Remove Hr
-                        </button>
-                    </div>
-                    {/* <div
-                        className="px-3 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
-                    >
-                        <button
+                        onClick={() => {
+                            navigate("/Admin")
+                        }}
 
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
-                            Appiled Jobs
+                            Home
                         </button>
-                    </div> */}
+                    </div>
 
                     <div
                         className="px-3 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
@@ -68,10 +68,8 @@ function Admin() {
                     </div>
                 </div>
             </div>
-            <div class="font-bold text-2xl">List of Available Jobs</div>
-
+            <div class="font-bold text-2xl">List of Hr Available</div>
             <div className="flex justify-center p-5">
-                
                 
                         
                     
@@ -83,18 +81,22 @@ function Admin() {
                                         <div key={index} className="p-2">
                                             <div class="max-w-md rounded overflow-hidden shadow-lg border-[2px] border-blue-900">
                                     <div class="px-6 pt-4">
-                                        <div class="font-bold text-2xl">{element.jobrole}</div>
+                                        <div class="font-bold text-2xl">{element.name}</div>
                                     </div>
                                     <div className="px-6 pt-1">
-                                        <p className="text-sm">{element.jobAddress}</p>
-                                        <p className="text-sm">{element.jobmode}</p>
-                                        <p className="text-sm font-bold pt-2">₹{element.jobsalary.from} - ₹{element.jobsalary.to} per year</p>
+                                        <p className="text-sm">{element.Age}</p>
+                                        <p className="text-sm">{element.desg}</p>
+                                        <p className="text-sm font-bold pt-2">{element.Experience}</p>
                                     </div>
                                     <div class="px-6 pt-4 pb-2">
-                                        <p>{element.jobspec}</p>
+                                        <p>{element.Domain}</p>
                                     </div>
                                     <div className="px-6 pt-1 flex justify-center pb-4">
                                         <div>
+                                            <button
+                                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                                Update
+                                            </button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                             <button
                                                 class="bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                                                 Remove
@@ -119,4 +121,4 @@ function Admin() {
         </div>
     )
 }
-export default Admin;
+export default Approve;
